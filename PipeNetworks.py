1d781643326cd8f13092d15aafb744c13938484e
AlexiosVavvas <alexios.vavvas@gmail.com>
2022-11-29 09:55:23 +0200

    Wrote NR method but i get det(Jac)~=0 (linalg.solve -> Singular matrix issue) (Code Not Working)

--- PipeNetworks.py
+++ PipeNetworks.py
@@ -1,33 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class Node:
-    number_of_nodes = 0
 
     def __init__(self, coordinates):
+        self.index = None
         self.x = coordinates[0]
         self.y = coordinates[1]
         if len(coordinates) == 3:
             self.z = coordinates[2]
         else:
             self.z = None
-        self.p0 = None
+        self.p0 = None  # pressure [Pa]
+        self.zeta = 0
         self.connectedTubes = []
         self.neighbouringNodes = []
-        Node.number_of_nodes += 1
 
 
 class Tube:
-    number_of_tubes = 0
 
     def __init__(self, connected_nodes):
-        self.length = -1
-        self.k = None
+        self.index = None
+        self.length = None
+        self.diameter = None
         self.Re = None
         self.Q = None
+        self.kappa = None
         self.lamda = None
+        self.zeta = 0
         self.connectedNodes = list(connected_nodes.astype(int))  # index of nodes at each end
-        Tube.number_of_tubes += 1
 
 
 class Network:
@@ -40,30 +42,42 @@ class Network:
         for i in range(len(nodes_input[:, 0])):
             coordinates = [nodes_input[i, 0], nodes_input[i, 1], nodes_input[i, 2]]
             self.nodes.append(Node(coordinates))
+            self.nodes[i].zeta = nodes_input[i, 3]
+            self.nodes[i].index = i
         for i in range(len(tubes_input[:, 0])):
             self.tubes.append(Tube(tubes_input[i, 0:2]))
             self.tubes[i].length = tubes_input[i, 2]
+            self.tubes[i].zeta = tubes_input[i, 3]
+            self.tubes[i].index = i
+        self.number_of_nodes = len(self.nodes)
+        self.number_of_tubes = len(self.tubes)
 
     # given a set of nodes and tubes,
     # it finds the specific tube that connects to each node
     def FindConnectedTubes(self):
-        for i in range(Tube.number_of_tubes):
+        for i in range(self.number_of_tubes):
             self.nodes[self.tubes[i].connectedNodes[0]].connectedTubes.append(i)
             self.nodes[self.tubes[i].connectedNodes[1]].connectedTubes.append(i)
 
     def FindNeighbouringElements(self):
-        for i in range(Node.number_of_nodes):
+        for i in range(self.number_of_nodes):
             for tube_index in self.nodes[i].connectedTubes:
                 if self.tubes[tube_index].connectedNodes[0] != i:
                     self.nodes[i].neighbouringNodes.append(self.tubes[tube_index].connectedNodes[0])
                 else:
                     self.nodes[i].neighbouringNodes.append(self.tubes[tube_index].connectedNodes[1])
 
+    def GetTubeIndexConnectingIwithJ(self, node_i_index: int, node_j_index: int):
+        for conn_tube_index in self.nodes[node_i_index].connectedTubes:
+            if self.tubes[conn_tube_index].connectedNodes[0] == node_j_index \
+                    or self.tubes[conn_tube_index].connectedNodes[1] == node_j_index:
+                return conn_tube_index
+
     def Plot(self):
         plt.figure()
         if self.nodes[0].z is not None:
             plt.axes(projection='3d')
-        for i in range(Tube.number_of_tubes):
+        for i in range(self.number_of_tubes):
             xs = [self.nodes[self.tubes[i].connectedNodes[0]].x, self.nodes[self.tubes[i].connectedNodes[1]].x]
             ys = [self.nodes[self.tubes[i].connectedNodes[0]].y, self.nodes[self.tubes[i].connectedNodes[1]].y]
             if self.nodes[0].z is not None:
@@ -76,12 +90,8 @@ class Network:
         plt.show()
 
 
-
-
-
-
-
-
-
-
-
+def sign(x, y):
+    if x >= y:
+        return 1
+    else:
+        return -1
--- main.py
+++ main.py
@@ -1,13 +1,81 @@
+from math import *
 from PipeNetworks import *
 
+# Creating network from file
 my_network = Network("nodes_input.csv", "tubes_input.csv")
 
+# Analyzing Topology
 my_network.FindConnectedTubes()
 my_network.FindNeighbouringElements()
 
-my_network.Plot()
-# for i in range(Node.number_of_nodes):
-#     print(f"Node {i} is connected with tubes: {nodes[i].connectedTubes}")
-# print()
-# for i in range(Node.number_of_nodes):
-#     print(f"Node {i} is connected neighbours: {nodes[i].neighbouringNodes}")
+# Manually initialize pressures
+my_network.nodes[0].p0 = 0.021e5
+init_p_drop_step = (0.021e5 - 0.001e5) / my_network.number_of_nodes
+for i in range(1, my_network.number_of_nodes):
+    my_network.nodes[i].p0 = abs(0.021e5 - init_p_drop_step * i)
+
+# Initial Diameter Guess
+d_0 = 0.04  # [m]
+epsilon = 0.05e3  # [m]
+for tube_ in my_network.tubes:
+    tube_.lamda = (1 / (1.14 - 2 * log10(epsilon / d_0))) ** 2
+    tube_.diameter = d_0
+    tube_.kappa = tube_.lamda * tube_.length / tube_.diameter + tube_.zeta
+
+# Newton-Raphson Method
+F = np.zeros(my_network.number_of_nodes)
+DF = np.zeros((my_network.number_of_nodes, my_network.number_of_nodes))
+deltaP = np.ones(my_network.number_of_nodes)
+
+P_guess = np.zeros(my_network.number_of_nodes)
+P_guess_old = np.copy(P_guess)
+# error = np.ones(my_network.number_of_nodes)
+
+for i, node in enumerate(my_network.nodes):
+    P_guess[i] = node.p0
+
+ITER = 100
+relax = 0.01
+
+iter_count = 0
+while (abs(deltaP) > 0.001).any() & (iter_count < ITER):
+
+    # Finding F() values
+    for i in range(my_network.number_of_nodes):
+        temp_F_sum = 0
+        for j in my_network.nodes[i].neighbouringNodes:
+            kappa = my_network.tubes[my_network.GetTubeIndexConnectingIwithJ(i, j)].kappa
+            temp_F = sign(P_guess[j], P_guess[i]) * sqrt(
+                abs(P_guess[j] - P_guess[i]) / kappa)
+            temp_F_sum += temp_F
+        F[i] = temp_F_sum
+
+    # Calculating derivatives DF(i, j)
+    for i in range(my_network.number_of_nodes):
+        temp_DF = 0
+        temp_DF_sum = 0
+        for j in my_network.nodes[i].neighbouringNodes:
+            if i != j:
+                kappa = my_network.tubes[my_network.GetTubeIndexConnectingIwithJ(i, j)].kappa
+                temp_DF = 1 / 2 / sqrt(kappa) / sqrt(abs(P_guess[j] - P_guess[i]))
+                DF[i, j] = temp_DF
+                temp_DF_sum += temp_DF
+
+        DF[i, i] = -temp_DF_sum
+
+    print(f"matrix DF at iter {iter_count} has det = {np.linalg.det(DF)}")
+    deltaP = np.linalg.solve(DF, F)
+    P_guess = (1 - relax) * P_guess_old + deltaP * relax
+    # error = P_guess - P_guess_old
+    P_guess_old = P_guess
+
+    # print(P_guess)
+    print(f"Done one loop, i: {iter_count} -> {iter_count + 1}")
+    iter_count += 1
+
+print(f"Loop Finished in {iter_count} iterations while error handling returns <<{(abs(deltaP) > 0.001).any()}>>\n")
+
+for i, node in enumerate(my_network.nodes):
+    node.p0 = P_guess[i]
+    print(f"Node {i} has P0 = {node.p0:.4f}")
+print(deltaP)
