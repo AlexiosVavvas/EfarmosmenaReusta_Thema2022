637bf4c6b4a56fd77b97f3cc19e516379a3ab66c
AlexiosVavvas <alexios.vavvas@gmail.com>
2022-12-05 09:00:49 +0200

    NR as a Network.function Reading standard diameters from file and iterating to change sizes (Needs improvements for speed, Check results)

--- PipeNetworks.py
+++ PipeNetworks.py
@@ -2,6 +2,13 @@ from math import *
 import matplotlib.pyplot as plt
 from ArrowPlot3d import *
 
+# Constants
+ro_gas = 0.79  # density [kg/m^3]
+ni_gas = 13.57e-6  # kinematic viscosity [m^2/s]
+ro_air = 1.225  # density [kg/m^3]
+g = 9.81  # gravity [m/s^2]
+epsilon = 0.05e3  # absolute roughness [m]
+
 
 class Node:
 
@@ -29,6 +36,7 @@ class Tube:
         self.diameter = None
         self.Re = None
         self.Q = None
+        self.u = None  # speed
         self.kappa = None
         self.lamda = None
         self.zeta = 0
@@ -61,6 +69,8 @@ class Network:
             self.tubes[i].diameter = tubes_input[i, 4]
         self.number_of_nodes = len(self.nodes)
         self.number_of_tubes = len(self.tubes)
+        self.max_p0_drop = None
+        self.max_p_drop = None
 
     # given a set of nodes and tubes,
     # it finds the specific tube that connects to each node
@@ -129,6 +139,119 @@ class Network:
             elif node_.consumption > 0:
                 ax.scatter(node_.x, node_.y, node_.z, color='blue')
         plt.show()
+
+    # noinspection PyPep8Naming
+    def SolveNR(self, ITER, relax, deltaP_stop, plot_flag: bool, print_flag: bool):
+        if print_flag:
+            print()
+            print(f"------------- NEWTON - RAPHSON -------------")
+        # Manually initialize static pressures
+        p_entrance = 0.025e5
+        self.nodes[0].p_static = p_entrance
+        p_drop_step = 0.2 * p_entrance / self.number_of_nodes
+        for i in range(1, self.number_of_nodes):
+            self.nodes[i].p_static = abs(p_entrance - p_drop_step * i)
+
+        # Calculating Tube Kappa
+        for tube_ in self.tubes:
+            tube_.lamda = (1 / (1.14 - 2 * log10(epsilon / tube_.diameter))) ** 2  # Jain's Equation for Re->Inf
+            tube_.kappa = tube_.lamda * tube_.length / tube_.diameter
+
+        # Newton-Raphson Method
+        F = np.zeros(self.number_of_nodes)
+        DF = np.zeros((self.number_of_nodes, self.number_of_nodes))  # Jacobian Matrix
+        DF[0, 0] = 1
+        deltaP = np.zeros(self.number_of_nodes)
+        deltaP_old = np.zeros(self.number_of_nodes)
+
+        p0_guess = np.zeros(self.number_of_nodes)
+        for i, node in enumerate(self.nodes):
+            p0_guess[i] = node.p_static
+
+        iter_count = 0
+        first_round = True
+        while (((sqrt(sum(abs(deltaP)))) / self.number_of_nodes > deltaP_stop) or first_round) & (
+                iter_count < ITER):
+
+            # Finding F() values
+            for i in range(1, self.number_of_nodes):
+                temp_F_sum = 0
+                for j in self.nodes[i].neighbouringNodes:
+                    kappa = self.tubes[self.GetTubeIndexConnecting_I_with_J(i, j)].kappa
+                    temp_F_sum += sign(p0_guess[j], p0_guess[i]) * sqrt(abs(p0_guess[i] - p0_guess[j]) / kappa)
+                F[i] = temp_F_sum + self.nodes[i].consumption
+
+            # Calculating derivatives DF(i, j)
+            for i in range(1, self.number_of_nodes):
+                temp_DF_sum = 0
+                for j in self.nodes[i].neighbouringNodes:
+                    kappa = self.tubes[self.GetTubeIndexConnecting_I_with_J(i, j)].kappa
+                    temp_DF = 1 / 2 / sqrt(kappa) / sqrt(abs(p0_guess[i] - p0_guess[j]))
+                    DF[i, j] = temp_DF
+                    temp_DF_sum += temp_DF
+
+                DF[i, i] = -temp_DF_sum
+
+            # Solving system
+            deltaP = np.linalg.solve(DF, -F)
+            deltaP = (1 - relax) * deltaP_old + deltaP * relax
+            p0_guess_old = p0_guess
+            p0_guess = p0_guess + deltaP
+
+            # plotting
+            if plot_flag:
+                for node2plot in range(0, self.number_of_nodes):
+                    plt.plot([iter_count, iter_count + 1], [p0_guess_old[node2plot], p0_guess[node2plot]], 'k-',
+                             linewidth=0.2)
+
+            iter_count += 1
+            first_round = False
+        if print_flag:
+            print(f"Loop Finished in {iter_count}/{ITER} iterations "
+                  f"while error handling returns <<{((sqrt(sum(abs(deltaP)))) / self.number_of_nodes > deltaP_stop)}>>\n")
+
+        # Saving converged results to network object variables
+        p_static = np.zeros(self.number_of_nodes)
+        for i, node in enumerate(self.nodes):
+            node.p0 = p0_guess[i]
+            node.p_static = node.p0 - (ro_gas - ro_air) * g * node.z
+            p_static[i] = node.p_static
+            if print_flag:
+                print(f"Node {i} has P0 = {node.p0:.5f}, \tP_static = {node.p_static} \tand final dP0 = {deltaP[i]}")
+
+        # showing final plot
+        if plot_flag:
+            plt.title("Newton-Raphson Method")
+            plt.xlabel("Iterations")
+            plt.ylabel("Total Pressures [Pa]")
+            plt.grid()
+            plt.draw()
+            plt.show()
+
+        # max pressure drop calc
+        self.max_p0_drop = (max(p0_guess) - min(p0_guess))  # [Pa]
+        self.max_p_drop = (max(p_static) - min(p_static))  # [Pa]
+        if print_flag:
+            print(f"Max total pressure drop: {self.max_p0_drop / 100:.8f} [mBar]")
+            print(f"Max static pressure drop: {self.max_p_drop / 100:.8f} [mBar]")
+            print()
+
+        # Calculating Q (+: from lower node index to higher)
+        #   e.g. if Q connecting node 2 and 3 is <0
+        #        that means that if flows from 3 -> 2
+        for tube_ in self.tubes:
+            i = tube_.connectedNodes[0]
+            j = tube_.connectedNodes[1]
+            Q = sign(self.nodes[i].p0, self.nodes[j].p0) * \
+                sqrt(abs(self.nodes[i].p0 - self.nodes[j].p0) / tube_.kappa)
+            if i > j:
+                Q *= -1
+            tube_.Q = Q
+            tube_.u = 8 * Q ** 2 * ro_gas / (pi ** 2 * tube_.diameter ** 4)
+
+            if print_flag:
+                print(f"Tube {tube_.index} (nodes {i} - {j}) ({tube_.diameter*1000:.2f}[mm])has \t"
+                      f"Q = {Q * 3600:.4f} [m^3/h]\t-> u = {tube_.u:.4f} [m/s]")
 
 
 def sign(x, y):
--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from PipeNetworks import *
 
-# Constants
-ro_gas = 0.79  # density [kg/m^3]
-ni_gas = 13.57e-6  # kinematic viscosity [m^2/s]
-ro_air = 1.225  # density [kg/m^3]
-g = 9.81  # gravity [m/s^2]
-epsilon = 0.05e3  # absolute roughness [m]
-
 # Creating network from file
 my_network = Network("nodes_input.csv", "tubes_input.csv")
 
@@ -14,110 +7,37 @@ my_network = Network("nodes_input.csv", "tubes_input.csv")
 my_network.FindConnectedTubes()
 my_network.FindNeighbouringElements()
 
-# Manually initialize static pressures
-p_entrance = 0.025e5
-my_network.nodes[0].p_static = p_entrance
-p_drop_step = 0.2 * p_entrance / my_network.number_of_nodes
-for i in range(1, my_network.number_of_nodes):
-    my_network.nodes[i].p_static = abs(p_entrance - p_drop_step * i)
+# First solve network with NR
+my_network.SolveNR(300, 0.1, 10 ** -5, False, False)
 
-# Calculating Tube Kappa
-for tube_ in my_network.tubes:
-    tube_.lamda = (1 / (1.14 - 2 * log10(epsilon / tube_.diameter))) ** 2  # Jain's Equation for Re->Inf
-    tube_.kappa = tube_.lamda * tube_.length / tube_.diameter
+# Getting standard diameters from file
+std_diam = np.genfromtxt("standard_diameters.csv", delimiter=',', skip_header=1)
+std_diam_len = len(std_diam)
 
-# Newton-Raphson Method
-F = np.zeros(my_network.number_of_nodes)
-DF = np.zeros((my_network.number_of_nodes, my_network.number_of_nodes))  # Jacobian Matrix
-DF[0, 0] = 1
-deltaP = np.zeros(my_network.number_of_nodes)
-deltaP_old = np.zeros(my_network.number_of_nodes)
+# Checking and Changing
+speed_goal = 4  # [m/s]
+max_p0_drop_goal = 200  # [Pa]
 
-p0_guess = np.zeros(my_network.number_of_nodes)
-for i, node in enumerate(my_network.nodes):
-    p0_guess[i] = node.p_static
+for i0 in range(50):
 
-ITER = 200
-relax = 0.1
-plot_flag = True
-deltaP_stop = 10 ** -4
+    for tube_ in my_network.tubes:
+        old_tube_ind = np.where((tube_.diameter - 0.003 < std_diam) * (std_diam < tube_.diameter + 0.003))[0][0]
+        if tube_.u > speed_goal:
+            new_tube_ind = old_tube_ind + 1
+            if new_tube_ind >= std_diam_len:
+                tube_.diameter = std_diam[std_diam_len - 1]
+            elif new_tube_ind <= 0:
+                tube_.diameter = std_diam[0]
+            else:
+                tube_.diameter = std_diam[new_tube_ind]
+        if tube_.u < speed_goal:
+            new_tube_ind = old_tube_ind - 1
+            if new_tube_ind >= std_diam_len:
+                tube_.diameter = std_diam[std_diam_len - 1]
+            elif new_tube_ind <= 0:
+                tube_.diameter = std_diam[0]
+            else:
+                tube_.diameter = std_diam[new_tube_ind]
 
+    my_network.SolveNR(300, 0.1, 10**-5, False, True)
 
-iter_count = 0
-first_round = True
-while (((sqrt(sum(abs(deltaP)))) / my_network.number_of_nodes > deltaP_stop) or first_round) & (iter_count < ITER):
-
-    # Finding F() values
-    for i in range(1, my_network.number_of_nodes):
-        temp_F_sum = 0
-        for j in my_network.nodes[i].neighbouringNodes:
-            kappa = my_network.tubes[my_network.GetTubeIndexConnecting_I_with_J(i, j)].kappa
-            temp_F_sum += sign(p0_guess[j], p0_guess[i]) * sqrt(abs(p0_guess[i] - p0_guess[j]) / kappa)
-        F[i] = temp_F_sum + my_network.nodes[i].consumption
-
-    # Calculating derivatives DF(i, j)
-    for i in range(1, my_network.number_of_nodes):
-        temp_DF = 0
-        temp_DF_sum = 0
-        for j in my_network.nodes[i].neighbouringNodes:
-            kappa = my_network.tubes[my_network.GetTubeIndexConnecting_I_with_J(i, j)].kappa
-            temp_DF = 1 / 2 / sqrt(kappa) / sqrt(abs(p0_guess[i] - p0_guess[j]))
-            DF[i, j] = temp_DF
-            temp_DF_sum += temp_DF
-
-        DF[i, i] = -temp_DF_sum
-
-    # Solving system
-    deltaP = np.linalg.solve(DF, -F)
-    deltaP = (1 - relax) * deltaP_old + deltaP * relax
-    p0_guess_old = p0_guess
-    p0_guess = p0_guess + deltaP
-
-    # plotting
-    if plot_flag:
-        for node2plot in range(0, my_network.number_of_nodes):
-            plt.plot([iter_count, iter_count + 1], [p0_guess_old[node2plot], p0_guess[node2plot]], 'k-', linewidth=0.2)
-
-    iter_count += 1
-    first_round = False
-print(f"Loop Finished in {iter_count}/{ITER} iterations "
-      f"while error handling returns <<{((sqrt(sum(abs(deltaP)))) / my_network.number_of_nodes > deltaP_stop)}>>\n")
-
-# Saving converged results to network object variables
-p_static = np.zeros(my_network.number_of_nodes)
-for i, node in enumerate(my_network.nodes):
-    node.p0 = p0_guess[i]
-    node.p_static = node.p0 - (ro_gas - ro_air) * g * node.z
-    p_static[i] = node.p_static
-    print(f"Node {i} has P0 = {node.p0:.5f}, \tP_static = {node.p_static} \tand final dP0 = {deltaP[i]}")
-
-# showing final plot
-if plot_flag:
-    plt.title("Newton-Raphson Method")
-    plt.xlabel("Iterations")
-    plt.ylabel("Total Pressures [Pa]")
-    plt.grid()
-    plt.draw()
-    plt.show()
-
-# max pressure drop calc
-print(f"Max total pressure drop: {(max(p0_guess) - min(p0_guess)) / 100:.8f} [mBar]")
-print(f"Max static pressure drop: {(max(p_static) - min(p_static)) / 100:.8f} [mBar]")
-print()
-
-# Calculating Q (+: from lower node index to higher)
-#   e.g. if Q connecting node 2 and 3 is <0
-#        that means that if flows from 3 -> 2
-for tube_ in my_network.tubes:
-    i = tube_.connectedNodes[0]
-    j = tube_.connectedNodes[1]
-    Q = sign(my_network.nodes[i].p0, my_network.nodes[j].p0) * \
-        sqrt(abs(my_network.nodes[i].p0 - my_network.nodes[j].p0) / tube_.kappa)
-    if i > j:
-        Q *= -1
-    tube_.Q = Q
-
-    print(f"Tube {tube_.index} (nodes {i} - {j}) has "
-          f"\tQ = {Q * 3600:.4f} [m^3/h]\t-> u = {8 * Q ** 2 * ro_gas / (pi ** 2 * tube_.diameter ** 4):.4f} [m/s]")
-
-my_network.DrawQ_withArrows()
